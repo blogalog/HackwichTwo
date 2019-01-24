dccb8034f5cfdc2dc6e3d06399a752a2a0fa821c
Brandon Logalog <blogalog@hawaii.edu>
2019-01-24 10:57:47 -1000

    Completed Hackwich #2

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func pressMeButtonPressed(_ sender: Any) {
+    print("Hello World")
+    }
+   
 
 
 }
